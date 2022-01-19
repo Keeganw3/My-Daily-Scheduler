3f03117e590fb2b2ca1020c5d8f8c39d372763d4
Keeganw3 <keeganw749@gmail.com>
2022-01-19 22:38:04 +0000

    Inputs must be a number in order for code to work

--- run.py
+++ run.py
@@ -7,8 +7,39 @@ def add_to_day_planner():
         print("Pick one number from 1-5 to select the option you want.")
         num = input("Enter your data here:")
         print(f"The option selected was {num}")
+        validate_data(num)
+
+        if validate_data(num):
+            print('number is valid!')
+            break
 
     return num
+
+def validate_data(values):
+    """
+    The try: converts all strings into integers.
+    Makes an error if strings cannot be converted into int.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 1:
+            raise ValueError(
+                f"1 value is required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+    
+#def update_sales_worksheet(data):
+#    """
+#    Update sales worksheet, add new row with the list data provided.
+#    """
+#    print("Updating sales worksheet...\n")
+#    sales_worksheet = SHEET.worksheet("sales")
+#   sales_worksheet.append_row(data)
+#    print("Sales worksheet updated successfully.\n")
 
 
 #def main()
