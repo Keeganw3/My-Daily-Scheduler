07ef1dfd61f57138c4962e08064c35a5a0f92e6c
Keeganw3 <keeganw749@gmail.com>
2022-01-20 03:47:12 +0000

    Added text for all possible options in day planner

--- run.py
+++ run.py
@@ -4,11 +4,25 @@ def add_to_day_planner():
     """
     while True:
         today = []
-        print("Pick one number from 1-5 to select the option you want.")
+        print("Pick one number from 1-7 to select the option you want.")
         num = input("Enter your data here:")
         print(f"The option selected was {num}")
         validate_data(num)
-
+        if int(num) == 1:
+            print("Eat lunch.")
+        elif int(num) == 2:
+            print("Go for a run.")
+        elif int(num) == 3:
+            print("Walk your pet.")
+        elif int(num) == 4:
+            print("Read a book.")
+        elif int(num) == 5:
+            print("Show more options.")
+        elif int(num) == 6:
+            print("Make your own option.")
+        else:
+            print("Exit day planner.")
+        
         if validate_data(num):
             print('number is valid!')
             break
@@ -21,7 +35,7 @@ def validate_data(values):
     Makes an error if strings cannot be converted into int.
     """
     try:
-        if int(values) >= 6 or int(values) <= 0:
+        if int(values) >= 8 or int(values) <= 0:
             raise ValueError(
                 f"A value from 1-5 is required, you provided {int(values)}"
             )
