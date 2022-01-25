344a17aa054da3b5f10bb67eb671ada931a06572
Keeganw3 <keeganw749@gmail.com>
2022-01-25 03:37:23 +0000

    Fixed bug with try except not accepting letters and made minor changes to the UI to make it easier for the user

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 def daily_planner():
     """
-    Allows user to options to the day planner. These options will be
+    Allows user to add options to the day planner. These options will be
     displayed to the user throughout their time using the code
     in a list and when they have exited the app.
     """
     today = []
-    print("Pick one number from 1-7 to select the option you want.")
+    print("Pick a number from 1-7 to select the option you want.")
     while True:
         if len(today) >= 3:
             print("Would you like to exit the daily planner?")
             user_input = input(f"Hit 'y' for yes and 'n' for no:\n ")
-            if user_input == 'y':
+            if user_input == "y":
                 print("Exiting daily planner...")
                 return today
-            elif user_input == 'n':
+            elif user_input == "n":
                 pass
             else:
                 print("'y' or 'n' wasn't selected. Please pick one.")
@@ -25,10 +25,16 @@ def daily_planner():
         print("4. Read a book")
         print("5. Show more options")
         print("6. Create new option")
-        print("7. Exit daily planner.")
+        print(f"7. Exit daily planner.\n")
+        
+        try:
+            num = int(input(f"Enter the number here:\n "))
+            print(f"The option selected was {num}")
+        except ValueError:
+            print("Invalid you didn't enter a number.")
+            input("Hit Enter to return to the main menu")
+            continue            
 
-        num = int(input(f"Enter the number here:\n "))
-        print(f"The option selected was {num}")
         if int(num) == 1:
             today.append("Eat lunch")
             print(today)
@@ -53,8 +59,6 @@ def daily_planner():
         elif int(num) == 7:
             print("Exiting daily planner...")
             return today
-        else:
-            validate_input(num)
     return today
 
 
@@ -66,26 +70,8 @@ def create_option():
     print("Type in your option below!")
     print("If this was a mistake type 'exit'.")
     print("to return to previous options")
-    user_made_option = input(f"\n")
+    user_made_option = input(f"")
     return user_made_option
-
-
-def validate_input(values):
-    """
-    The try: converts all strings into integers.
-    Makes an error if strings cannot be converted into int.
-    """
-    while True:
-        try:
-            if values < 8 or values > 0:
-                raise ValueError(
-                    f"A value from 1-7 is required, you provided {int(values)}"
-                )
-        except ValueError as e:
-            print(f"Invalid data: {e}, please try again.\n")
-            return True
-
-        return True
 
 
 def main():
@@ -97,12 +83,13 @@ def main():
     print("I hope you enjoyed!")
 
 
-print("Welcome! This is a Day Planner!")
-print("Below are some suggestions for what you may want to do today.")
-print("You can also make your own suggestions!")
-print("When 3 items are in the day planner the user be asked if they're done.")
-print("If you say yes, your list will be shown to you.")
-print("Otherwise, you will be able to add more to the day planner.")
-start = input("Hit Enter when you're ready to start the daily planner!")
+if __name__:
+    print("Welcome! This is a Daliy Planner!")
+    print("Below are some suggestions for what you may want to do today.")
+    print("You can also make your own suggestions!")
+    print("When 3 tasks are in your day planner you will be asked if you're done.")
+    print("If you say yes, your list will be shown to you.")
+    print(f"Otherwise, you will be able to add more to the day planner.\n")
+    input(f"Hit Enter when you're ready to start the daily planner!\n")
+    main()
 
-main()
